464fe49f63783ed9e1f9562d30e8d04c1386bd37
Emmanuel Nsikan-David <en56434@gmail.com>
2025-02-02 21:29:41 +0100

    finished stack section

--- src/Components/Stack/Stack.tsx
+++ src/Components/Stack/Stack.tsx
@@ -1,18 +1,70 @@
+import "./Stack.css"
+import htmlIcon from "./assets/html-icon.svg"
+import cssIcon from "./assets/css-icon.svg"
+import jsIcon from "./assets/js-icon.svg"
+import tsIcon from "./assets/ts-icon.svg"
+import reactIcon from "./assets/react-icon.svg"
+import nodeIcon from "./assets/node-icon.svg"
+import expressIcon from "./assets/express-icon.svg"
+import mongoIcon from "./assets/mongo-icon.svg"
+import figmaIcon from "./assets/figma-icon.svg"
+
+const stackInfo = [{
+    name : "HTML",
+    icon : htmlIcon
+    },
+    {
+        name : "CSS",
+        icon : cssIcon
+    },
+    {
+        name : "JavaScript",
+        icon : jsIcon
+    },
+    {
+        name : "TypeScript",
+        icon : tsIcon
+    },
+    {
+        name : "React.JS",
+        icon : reactIcon
+    },
+    {
+        name : "Node.JS",
+        icon : nodeIcon
+    },
+    {
+        name : "Express.JS",
+        icon : expressIcon
+    },
+    {
+        name : "MongoDB",
+        icon : mongoIcon
+    },
+    {
+        name : "Figma",
+        icon : figmaIcon
+    }
+]
+
 function Stack(){
+    const allStack = stackInfo.map(({icon, name})=>{
+        return <div className="single-stack">
+        <img src={icon} alt="icon" />
+        
+        <h2 className={name == "Figma" ? "italic" : ""}>{name}</h2>
+    </div>
+    })
     return(
-        <section className="stack-background">
+        <section className="stack-section">
+            <div className="inner-wrapper">
+
             <h1 className="main-heading">My Stack</h1>
 
             <div className="stack-container">
-                <div className="single-stack">
-                    <img src="" alt="icon" />
-                    <h2>HTML</h2>
-                </div>
+                {allStack}
+            </div>
 
-                <div className="single-stack">
-                    <img src="" alt="icon" />
-                    <h2>HTML</h2>
-                </div>
             </div>
         </section>
     )
