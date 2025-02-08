d9da8d6de1fe05e72c9e6ac2715ef07ac3ee8e14
Emmanuel Nsikan-David <en56434@gmail.com>
2025-02-08 21:31:58 +0100

    finished about me section

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import "./About.css"
+import meSvg from "./assets/me.svg"
 
 function About() {
   return (
     <section className='about-section'>
-        <h1>About Me</h1>
+      <div className="inner-wrapper">
 
-        <h2>Hey there [wave emoji]</h2>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium cumque modi voluptate odit quibusdam iste dolores ex quisquam possimus? Maiores, numquam eaque. Impedit corporis dolore quod, perspiciatis, minima dolor corrupti repudiandae commodi reiciendis delectus aliquid expedita dignissimos nulla. Voluptate reprehenderit cupiditate sed doloribus explicabo eaque sapiente blanditiis ratione veritatis aut autem error nam odit quidem, exercitationem ipsa, ipsum porro deserunt laudantium possimus aliquid. Perferendis molestiae ratione assumenda dignissimos quidem sit maxime mollitia quia, sunt necessitatibus nobis ut, tempora vero odit.</p>
+        <h1 className="main-heading">About Me</h1>
+ 
+        <h2 className="sub-heading">Hey there 👋🏾</h2>
 
-        <img src="" alt="image of Nsikan" />
+        <p className="my-description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium cumque modi voluptate odit quibusdam iste dolores ex quisquam possimus? Maiores, numquam eaque. Impedit corporis dolore quod, perspiciatis, minima dolor corrupti repudiandae commodi reiciendis delectus aliquid expedita dignissimos nulla. Voluptate reprehenderit cupiditate sed doloribus explicabo eaque sapiente blanditiis ratione veritatis aut autem error nam odit quidem, exercitationem ipsa, ipsum porro deserunt laudantium possimus aliquid. Perferendis molestiae ratione assumenda dignissimos quidem sit maxime mollitia quia, sunt necessitatibus nobis ut, tempora vero odit.</p>
+
+        <img src={meSvg} alt="image of Nsikan" className="my-image" />
+
+      </div>
+
     </section>
   )
 }
