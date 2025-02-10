7d4266a9b3d67b4943c3f51bbca0f275f169587a
Emmanuel Nsikan-David <en56434@gmail.com>
2025-02-10 23:45:52 +0100

    minor changes

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -3,24 +3,67 @@ import menuIcon from "./assets/menu-icon.svg"
 import resumeIcon from "./assets/resume-icon.svg"
 import resume from "./assets/resume.pdf"
 import meIcon from "./assets/me-icon.png"
+import { useState, useRef, useEffect } from "react"
 
 function Hero(){
+    const [showMobileMenu, setShowMobileMenu] = useState(false)
+    const headerRef = useRef(null)
+    useEffect(()=>{
+        makeHeaderStick()
+    }, [])
+    function closeOnClick(){
+        setShowMobileMenu(false)
+    }
+
+    function makeHeaderStick(){
+        window.addEventListener("scroll", function() {
+            if (window.scrollY > 100) { // Adjust scroll position as needed
+                if(headerRef.current){
+                    (headerRef.current as HTMLElement).classList.add("sticky");
+                }}
+            else{
+                if(headerRef.current){
+                    (headerRef.current as HTMLElement).classList.remove("sticky");
+                }
+            }
+        });
+    }
+    const mappedNavLinks = (
+        <>
+        <a 
+        onClick={closeOnClick}
+        href="#home">Home</a>
+        <a 
+        onClick={closeOnClick}
+        href="#projects">Projects</a>
+        <img src={meIcon} alt="image of Nsikan-David" />
+        <a 
+        onClick={closeOnClick}
+        href="#about">About</a>
+        <a 
+        onClick={closeOnClick}
+        href="#contact">Contact Me</a>
+        </>
+    )
+
     return (
         <section id="home" className="hero-section">
             <div className="inner-wrapper">
-            <header>
-            <nav>
+            <header ref={headerRef}>
+            <nav className={showMobileMenu ? "show" : ""}>
                 <ul>
-                <button>X</button>
+                <button onClick={()=>{
+                    setShowMobileMenu(false)
+                }}>X</button>
 
-                    <a href="#home">Home</a>
-                    <a href="#projects">Projects</a>
-                    <img src={meIcon} alt="image of Nsikan-David" />
-                    <a href="#about">About</a>
-                    <a href="#contact">Contact Me</a>
+                    {mappedNavLinks}
                 </ul>
             </nav>
-            <button className="menu-btn">
+            <button
+            onClick={()=>{
+                setShowMobileMenu(true)
+            }}
+            className="menu-btn">
                 <img src={menuIcon} alt="menu icon" />
             </button>
         </header>
